330e7a1b3a73849920a582d083bebbbfa950f40d
Amrit Lal Paswan <amritpaswan5952@gmail.com>
2025-06-23 02:12:11 +0530

    Profile

---
+++ Frontend/src/pages/Chat.tsx
@@ -0,0 +1,11 @@
+import { ChatLayout } from "@/components/chat/ChatLayout";
+
+const Chat = () => {
+  return (
+    <div className="h-screen bg-gray-50">
+      <ChatLayout />
+    </div>
+  );
+};
+
+export default Chat;
